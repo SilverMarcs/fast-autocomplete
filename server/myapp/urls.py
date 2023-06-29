73084875b83e7002fb5b4bb110801107fa9571d0
Zabir Raihan <zabirraihan6@gmail.com>
2023-06-30 00:17:53 +0600

    Simplify by putting everything in server folder

--- server/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('insert', views.insertWord, name='insert'),
-    path('autocomplete', views.autocomplete, name='autocomplete'),
-    path('reset', views.reset, name='reset'),
-]
