3ac5160ea2633dfbcdc5cd611581ff0cf6efee79
Zabir Raihan <zabirraihan6@gmail.com>
2023-07-02 12:20:59 +0600

    Loading spinner if reset takes long during page load

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
-import { Box, ChakraProvider, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
@@ -7,17 +14,43 @@ import TrainingForm from "./components/TrainingForm";
 
 function App() {
   const [reset, setReset] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // Resetting the model everytime page is refreshed/loaded because not using a database atm
   useEffect(() => {
     const resetModel = async () => {
-      await axios.post(`${process.env.REACT_APP_API_URL}/reset`);
+      setIsLoading(true);
+      try {
+        await axios.post(`${process.env.REACT_APP_API_URL}/reset`);
+      } catch (error) {
+        console.error(error);
+      }
+      setIsLoading(false);
     };
     resetModel();
   }, []);
 
   return (
     <ChakraProvider>
+      {isLoading && (
+        <Box
+          position="fixed"
+          top="0"
+          right="0"
+          bottom="0"
+          left="0"
+          backgroundColor="rgba(0, 0, 0, 0.5)"
+          zIndex="1000"
+        >
+          <Center height="100%">
+            <VStack spacing={4}>
+              <Spinner size="xl" color="blue.500" />
+              <Text fontSize="large" color="white">
+                Loading Goodies
+              </Text>
+            </VStack>
+          </Center>
+        </Box>
+      )}
       <Box p={8} color="white" bg="gray.800">
         <Text fontSize="2xl" fontWeight="bold" color="blue.300">
           Welcome to the Autocomplete Model Trainer!
