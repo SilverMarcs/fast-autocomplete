c1324afe58796db55cd7747baff552c514f8d529
Zabir Raihan <zabirraihan6@gmail.com>
2023-07-02 11:13:41 +0600

    rename restalertdialogbtn, move, handle reset in trainingform, and temp remove resetting InputBases on reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,18 @@
-import { Box, ChakraProvider, Text, useToast } from "@chakra-ui/react";
+import { Box, ChakraProvider, Text } from "@chakra-ui/react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import SuggestionInput from "./components/SuggestionInput";
 import TrainingForm from "./components/TrainingForm";
 
 function App() {
-  const [reset, setReset] = useState(false);
-
-  const toast = useToast();
-
   // Resetting the model everytime page is refreshed/loaded because not using a database atm
   useEffect(() => {
-    const fetchData = async () => {
+    const resetModel = async () => {
       await axios.post(`${process.env.REACT_APP_API_URL}/reset`);
     };
-    fetchData();
+    resetModel();
   }, []);
-
-  const handleReset = async () => {
-    try {
-      await axios.post(`${process.env.REACT_APP_API_URL}/reset`);
-      toast({
-        title: "Successfully reset autocomplete model.",
-        status: "success",
-        duration: 1700,
-        isClosable: true,
-      });
-      // setTrainingText("");
-      setReset(true);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   return (
     <ChakraProvider>
@@ -40,14 +20,11 @@ function App() {
         <Text fontSize="2xl" fontWeight="bold" color="blue.300">
           Welcome to the Autocomplete Model Trainer!
         </Text>
-        <TrainingForm onReset={handleReset} />
+        <TrainingForm />
       </Box>
 
       <Box p={8} bg="gray.800">
-        <SuggestionInput
-          reset={reset}
-          onResetComplete={() => setReset(false)}
-        />
+        <SuggestionInput />
 
         <Text mt={100} fontStyle={"italic"} color="gray.500">
           *Model is reset on page refresh
