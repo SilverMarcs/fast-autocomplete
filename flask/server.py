e6482a6822bbbe1e7699e33d35f3ffa0d29c637f
Zabir Raihan <zabirraihan6@gmail.com>
2023-07-18 01:41:46 +0600

    cleanup

--- flask/server.py
+++ flask/server.py
@@ -1,39 +1,15 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 from trie import Trie
 import json
 
-# app = Flask(__name__)
-# CORS_ALLOW_ORIGIN = "*,*"
-# CORS_EXPOSE_HEADERS = "*,*"
-# CORS_ALLOW_HEADERS = "content-type,*"
-# cors = CORS(
-#     app,
-#     origins=CORS_ALLOW_ORIGIN.split(","),
-#     allow_headers=CORS_ALLOW_HEADERS.split(","),
-#     expose_headers=CORS_EXPOSE_HEADERS.split(","),
-#     supports_credentials=True,
-# )
-
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config["CORS_HEADERS"] = "Content-Type"
-
-
-@app.route("/insert", methods=["OPTIONS"])
-@cross_origin()
-def handle_options():
-    response = jsonify({"status": "OK"})
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
-    response.headers.add("Access-Control-Allow-Methods", "POST")
-    return response
 
 
 @app.route("/insert", methods=["POST"])
-@cross_origin()
 def insert_word():
     data = json.loads(request.data)
     sentence = data.get("words")
@@ -46,7 +22,6 @@ def insert_word():
 
 
 @app.route("/autocomplete", methods=["POST"])
-@cross_origin()
 def autocomplete():
     data = json.loads(request.data)
     prompt = data.get("prompt")
@@ -57,7 +32,6 @@ def autocomplete():
 
 
 @app.route("/reset", methods=["POST"])
-@cross_origin()
 def reset():
     trie = Trie.Instance()
     trie.reset()
